fae4eb33de2fb2ccbb7e97edca30b779976bd40b
Rick Scavetta <office@scavetta.academy>
2020-11-13 12:35:34 +0100

    Add files via upload

--- 5 - SILAC Analysis.R
+++ 5 - SILAC Analysis.R
@@ -24,14 +24,33 @@ test_tb$`protein size (KD)`
 
 # Quantify the contaminants ----
 
+protein_df %>%
+  filter(Contaminant == "+") %>% 
+  nrow()
+
+# recall TRUE is 1
+sum(protein_df$Contaminant == "+")
+
+# for categorical variables (i.e. "factors") or even characters
+table(protein_df$Contaminant)
 
 # Proportion of contaminants
+sum(protein_df$Contaminant == "+")/nrow(protein_df)
+
+# length(protein_df) # caution, this is the number of columns, not rows
 
 # Percentage of contaminants (just multiply proportion by 100)
+sum(protein_df$Contaminant == "+")/nrow(protein_df) * 100
 
 # Transformations & cleaning data ----
 
 # Remove contaminants ====
+
+protein_df[protein_df$Contaminant != "+",]
+
+protein_df %>% 
+  filter(Contaminant != "+")
+
 
 
 # log 10 transformations of the intensities ====
@@ -56,15 +75,19 @@ test_tb$`protein size (KD)`
 #   mutate_at(vars(starts_with("Rat"), -ends_with("Sig")), log2)
 
 # A bit newer way, using across:
-protein_df <- protein_df %>% 
+# protein_df <- read.delim("data/Protein.txt")
+protein_df <- protein_df %>%
   as_tibble() %>% 
+  filter(Contaminant != "+") %>% 
   mutate(across(starts_with("Rat") & -ends_with("Sig"), log2)) %>% 
   mutate(across(starts_with("Int"), log10)) %>% 
   mutate(Intensity.H.M = Intensity.H + Intensity.M,
          Intensity.M.L = Intensity.M + Intensity.L)
 
 
-
+# This version would make NEW columns with a suffix "_hello" that contained the 
+# transformed values:
+# mutate(across(starts_with("Rat") & -ends_with("Sig"), list(hello = log2))) %>% 
 
 
 # problems here:
@@ -84,17 +107,46 @@ protein_df <- protein_df %>%
 
 
 # Part 2: Query data using filter() ----
-# Exercise 9.2 (Find protein values) ====
+# Exercise 9.3 (Find protein values) ====
+# Ratios for (H/M, M/L) of these Uniprot:
+
+query <- c("GOGA7", "PSA6", "S10AB")
+
+protein_df %>% 
+  filter(Uniprot %in% query)
+
+# solutions:
+# 1 - add "_MOUSE" to our query
+query_long <- paste0(query, "_MOUSE")
+
+protein_df %>% 
+  filter(Uniprot %in% query_long)
 
 
+# 2 - remove _"MOUSE" from the search space
 
+# 3 - Use pattern matching
 
+query_2 <- c("Q3TFA5", "Q3UR67", "ADPRH") 
+
+protein_df %>% 
+  filter(Uniprot %in% query_2)
+
+protein_df %>% 
+  filter(str_detect(Uniprot, "Q3TFA5|Q3UR67|ADPRH"))
+
+# get all ubiquitins:
+protein_df %>% 
+    filter(str_detect(Description, "ubiquitin"))
 
 # Exercise 9.3 (Find significant hits) and 10.2 ====
 # For the H/M ratio column, create a new data 
 # frame containing only proteins that have 
 # a p-value less than 0.05
 
+# use filter()
+protein_df %>% 
+  filter(Ratio.H.M.Sig <= 0.05)
 
 # Exercise 10.4 (Find top 20 values) ==== 
 
